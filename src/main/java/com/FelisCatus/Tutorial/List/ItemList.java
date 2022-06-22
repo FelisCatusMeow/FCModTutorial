0e8eabcee9966e3990ca097681da7e281ebb18c0
FelisCatusMeow <2374512353@qq.com>
2022-06-22 22:50:35 +0800

    Tooltip & Armor

--- src/main/java/com/FelisCatus/Tutorial/List/ItemList.java
+++ src/main/java/com/FelisCatus/Tutorial/List/ItemList.java
@@ -2,6 +2,9 @@ package com.FelisCatus.Tutorial.List;
 
 import com.FelisCatus.Tutorial.ToolItem.PinkSword;
 import com.FelisCatus.Tutorial.Tutorial;
+import com.FelisCatus.Tutorial.Util.TutorialArmorMaterial;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
@@ -15,7 +18,19 @@ public class ItemList
             () -> new Item(new Item.Properties().food(FoodList.SOUP).group(Tutorial.TUTORIAL_GROUP)));
     public static final RegistryObject<Item> PINK_SWROD = ITEMS.register("pink_sword", PinkSword::new);
 
-    public static final RegistryObject<Item> WOW = ITEMS.register("wow", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+    public static final RegistryObject<Item> WOW = ITEMS.register("wow",
+            () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
 
-
+    public static final RegistryObject<Item> CHROME_BOOTS = ITEMS.register("chrome_boots",
+            () -> new ArmorItem(TutorialArmorMaterial.CHROME, EquipmentSlotType.FEET,
+                    new Item.Properties().group(Tutorial.TUTORIAL_GROUP)));
+    public static final RegistryObject<Item> CHROME_HELMET = ITEMS.register("chrome_helmet",
+            () -> new ArmorItem(TutorialArmorMaterial.CHROME, EquipmentSlotType.HEAD,
+                    new Item.Properties().group(Tutorial.TUTORIAL_GROUP)));
+    public static final RegistryObject<Item> CHROME_LEGGINGS = ITEMS.register("chrome_leggings",
+            () -> new ArmorItem(TutorialArmorMaterial.CHROME, EquipmentSlotType.LEGS,
+                    new Item.Properties().group(Tutorial.TUTORIAL_GROUP)));
+    public static final RegistryObject<Item> CHROME_CHESTPLATE = ITEMS.register("chrome_chestplate",
+            () -> new ArmorItem(TutorialArmorMaterial.CHROME, EquipmentSlotType.CHEST,
+                    new Item.Properties().group(Tutorial.TUTORIAL_GROUP)));
 }
--- src/main/java/com/FelisCatus/Tutorial/ToolItem/PinkSword.java
+++ src/main/java/com/FelisCatus/Tutorial/ToolItem/PinkSword.java
@@ -2,9 +2,18 @@ package com.FelisCatus.Tutorial.ToolItem;
 
 import com.FelisCatus.Tutorial.Util.CustomItemTier;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class PinkSword extends SwordItem {
 
@@ -12,5 +21,20 @@ public class PinkSword extends SwordItem {
     {
         super(CustomItemTier.ExampleTier, 8, -2.4F,new Item.Properties().group(ItemGroup.COMBAT));
     }
-    
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn)
+    {
+        if(Screen.hasShiftDown())
+        {
+            tooltip.add(new TranslationTextComponent("tooltip.tutorial.pink_sword_shift"));
+
+        }else
+        {
+            tooltip.add(new TranslationTextComponent("tooltip.tutorial.pink_sword"));
+        }
+
+
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+    }
 }
